c18c323ac909c9731336a1d7604777c9976d032f
AndNasPlay <57287272+AndNasPlay@users.noreply.github.com>
2020-06-27 14:57:30 +0300

    Lesson 7

--- Lesson7_Shchekatunov.playground/Contents.swift
+++ Lesson7_Shchekatunov.playground/Contents.swift
@@ -3,17 +3,15 @@ import UIKit
 struct Car {
     var price: Int
     var count: Int
-    var models: Models
     var equipment: Equipment
     let product: Product
     
 }
 struct Product{
-    let name: String
+    let name: Model
 }
-
-enum Models {
-    case Sportage, Stinger, Ceed
+enum Model {
+    case Sportage, Ceed
 }
 enum Equipment {
     case Comfort, Luxe, Prestige
@@ -25,29 +23,49 @@ enum FactoryError: Error {
 }
 
 class Factory {
-    var stock = ["Sportage Comfort":Car(price: 12000, count: 20, models: .Sportage, equipment: .Comfort, product: Product(name: "Sportage Comfort")),
-                 "Sportage Luxe":Car(price: 14000, count: 20, models: .Sportage, equipment: .Luxe, product: Product(name: "Sportage Luxe")),
-                 "Stinger Luxe":Car(price: 20000, count: 10, models: .Stinger, equipment: .Luxe, product: Product(name: "Stinger Luxe")),
-                 "Ceed Prestige":Car(price: 8000, count: 40, models: .Ceed, equipment: .Prestige, product: Product(name: "Ceed Prestige"))
+    private var stock = ["Sportage Comfort": Car(price: 10000, count: 3, equipment: .Comfort, product: Product(name: .Sportage)),
+                         "Sportage Luxe": Car(price: 12000, count: 2, equipment: .Luxe, product: Product(name: .Sportage)),
+                         "Ceed Prestige": Car(price: 9000, count: 10, equipment: .Prestige, product: Product(name: .Ceed))
     ]
     var cash = 0
-    func carPurchase(carModel name: String) throws -> (Product?, FactoryError?) {
-        guard let Car = stock[name] else {
+    func vend(carModel name: String) throws -> Product {
+        guard let car = stock[name] else {
             throw FactoryError.invalidSelection
-            
         }
-        guard Car.count > 0 else {
+        
+        guard car.count > 0 else {
             throw FactoryError.outOfStock
         }
-        guard Car.price <= cash else {
-            throw FactoryError.insufficientFunds(cashNeeded: Car.price - cash)
+        
+        guard car.price <= cash else {
+            throw FactoryError.insufficientFunds(cashNeeded: car.price - cash )
         }
-        cash -= Car.price
-        var newCar = Car
+        
+        cash -= car.price
+        var newCar = car
         newCar.count -= 1
         stock[name] = newCar
+        
+        print("Вы купили \(name) остаток средств \(cash) ")
         return newCar.product
     }
+}
+let BuyCar = Factory()
+do {
+    BuyCar.cash = 20000
+    try BuyCar.vend(carModel: "Sportage Luxe")
+}
+catch FactoryError.outOfStock{
+    print("Товар закончился на складе")
+}
+catch FactoryError.invalidSelection {
+    print("Такая комплектация отсутствует")
+}
+catch FactoryError.insufficientFunds(let cashNeeded) {
+    print("Недостаточно средств, доплатите \(cashNeeded)")
+}
+catch let error {
+    print(error.localizedDescription)
 }
 
 
